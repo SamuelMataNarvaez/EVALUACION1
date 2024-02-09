1f8e605f488baef568b32b28b674b6047eb672dd
SamuelMataNarvaez <159494333+SamuelMataNarvaez@users.noreply.github.com>
2024-02-09 16:50:37 -0600

    Add files via upload

---
+++ EVA1_2_IDENTIFICADORES/src/eva1_2_identificadores/EVA1_2_IDENTIFICADORES.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_identificadores;
+
+/**
+ *
+ * @author user
+ */
+public class EVA1_2_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //VARIABLES:
+        //TIPO DE DATO+NOMBRE DEL IDENTIFICADOR
+        //SHORT INT BYE LONG --> ENTEROS
+        //FLOAT DOUBLE-->NUMEROS CON DECIMAL
+        //CHAR-->CHARACTER-->CARACTERES
+        //BOOLEAN-->VERDADERO O FALSO
+        //STRING--> CADENAS DE TEXTO
+        int edad;//declaracion de una variable
+        String nombre;
+        double salario;
+        //ASIGNACION DE VALORES:
+        //IDENTIFICADOR = VALOR;
+        edad = 19;
+        nombre = "Samuel Mata Narvaez";
+        salario = 123.3;
+        //COMO LOS USAMOS:
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+    }
+    
+}
