3b29702251bb99b30bd5bd4eac61197fd4a38435
SamuelMataNarvaez <159494333+SamuelMataNarvaez@users.noreply.github.com>
2024-02-15 17:01:47 -0600

    Add files via upload

---
+++ EVA1_4_ASIGNACION/src/eva1_4_asignacion/EVA1_4_ASIGNACION.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_asignacion;
+
+/**
+ *
+ * @author user
+ */
+public class EVA1_4_ASIGNACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad; //DECLARACION
+        edad = 19; //ASIGNACION --> UN SOLO = SIGNIFICA ASIGNAR
+        System.out.println("Tu edad es:");
+        System.out.println(edad);
+         String carrera;
+         carrera = "Sistemas Computacionales";
+          System.out.println("Tu carrera es");
+        System.out.println(carrera);
+        String nombre;
+         nombre = "Samuel Mata Narvaez";
+          System.out.println("Tu nombre es");
+        System.out.println(nombre);
+    }
+    
+}
